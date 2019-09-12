d3cce89a6961431f54711efccd2b8c15cb015cb1
whatasoda <git@whatasoda.me>
2019-09-12 19:19:49 +0900

    test: initial

---
+++ __tests__/samples.test.ts
@@ -0,0 +1,15 @@
+import { CLIEngine } from 'eslint';
+import path from 'path';
+
+const cli = new CLIEngine({
+  cwd: path.resolve(__dirname, '../'),
+  extensions: ['ts', 'tsx'],
+});
+
+it('snapshot', () => {
+  const report = cli.executeOnFiles(['./samples/']);
+  report.results.forEach((result) => {
+    delete result.source;
+  });
+  expect(report).toMatchSnapshot();
+});
---
+++ samples/index.ts
@@ -0,0 +1,9 @@
+// TODO: enrich
+
+type SomeFuncType = (arg: any) => void;
+
+const someFunc: SomeFuncType = (_) => {};
+
+const unused = null;
+
+export { someFunc };
